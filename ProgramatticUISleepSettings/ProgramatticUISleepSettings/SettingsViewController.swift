18b2e235db1f3e288f6b6dbeedbb3f011cd642d4
JB184351 <justinbengtson@icloud.com>
2020-10-10 21:41:52 -0600

    Table View is being presented

--- ProgramatticUISleepSettings/ProgramatticUISleepSettings/SettingsViewController.swift
+++ ProgramatticUISleepSettings/ProgramatticUISleepSettings/SettingsViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        tableView.dataSource = self
+        tableView.delegate = self
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        // This is how we get our tableView in our viewController
+        view.addSubview(tableView)
+        tableView.rowHeight = 50
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
 
+}
+
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension SettingsViewController: UITableViewDelegate {
+    
 }
 
